8545e3aea76a54f10d7795abf5cbb7798b4b0814
Pandachu96 <yangzebang@hotmail.com>
2026-09-20 13:39:27 -0400

    labs commit

---
+++ Labs/F26-Lab1/lab1a.py
@@ -0,0 +1,30 @@
+# Add comments before you do anything else.
+
+#!/usr/bin/env python3
+# Author: Zebang Yang
+# Date: 9/20/2026
+# Purpose: Create a variable, check its type and print the variable.
+# Usage: python3 lab1a.py
+
+# TO DO 1: Creating and using varibales
+# create a variable called message.
+# Set the variable to equal to "Welcome to PRG101".
+# Print the variable message using print() statement.
+
+# TO DO 2: Checking the type of a varibale
+# Use the builtin type() function and print the type of this variable.
+
+# TO DO 3: Dynamic Typing:
+# Create a varibel called `x` and assign it the value 10, then print the type of this variable.
+
+# TO DO 4: Dynamic Typing: 
+# Now reassign a new value to the variable `x`, this value should be a string, e.g "hello", check the type of the variable `x` again.
+# What did you observe?
+
+x = 10
+print(type(x))
+
+x = 'Hello'
+print(type(x))
+
+# The type of x is reassigned from int to string because the value was overridden in the memory.
